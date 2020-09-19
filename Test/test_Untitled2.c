6eb3cf7b38da09970440eb77d4d48a1d33980b9f
99002513 <71481367+99002513@users.noreply.github.com>
2020-09-19 11:41:08 +0530

    Update test_Untitled2.c

--- Test/test_Untitled2.c
+++ Test/test_Untitled2.c
@@ -43,32 +43,33 @@ int main() {
 /* Note: Do not edit END */
   return 0;
 }
+/*
 
-/* Write all the test functions */ 
 void test_add(void) {
   CU_ASSERT(30 == add(10, 20));
   
-  /* Dummy fail*/
+ 
   CU_ASSERT(1500 == add(750, 7500));
 }
 
 void test_subtract(void) {
   CU_ASSERT(-3 == subtract(0, 3));
   
-  /* Dummy fail*/
+ 
   CU_ASSERT(1 == subtract(1000, 900));
 }
 
 void test_multiply(void) {
   CU_ASSERT(0 == multiply(1, 0));
   
-  /* Dummy fail*/
+  /
   CU_ASSERT(2 == multiply(2, 5));
 }
 
 void test_divide(void) {
   CU_ASSERT(0 == divide(1, 0));
   
-  /* Dummy fail*/
+  /
   CU_ASSERT(3 == divide(2, 2));
-}
+}"
+  */
